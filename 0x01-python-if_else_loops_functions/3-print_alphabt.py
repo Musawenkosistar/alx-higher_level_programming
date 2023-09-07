39030005ac39719eb904078c17d23c894db66a52
Musawenkosistar <musawenkosi2504@gmail.com>
2023-09-07 04:28:13 -0700

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number > 0:
-    print('{:d} is positive'.format(number))
-elif number == 0:
-    print('{:d} is zero'.format(number))
-elif number < 0:
-    print('{:d} is negative'.format(number))
+for c in range(97, 123):
+    if c != 101 and c != 113:
+        print('{}'.format(chr(c)), end='')
