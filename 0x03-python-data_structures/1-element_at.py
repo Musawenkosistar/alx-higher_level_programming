74b5aaa32c35bda994b15908a86a53cf17989cd2
Musawenkosistar <musawenkosi2504@gmail.com>
2023-09-11 23:38:26 -0700

    0x03-python-data_structures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+# 1-element_at.py
+
+
 def element_at(my_list, idx):
-    for x in my_list:
-        if idx == my_list.index(x):
-            return(x)
-        elif idx > len(my_list):
-            return None
-        elif idx < 0:
-            return None
+    """Retrive an element from a list."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
+
